3362c1ec09e736be05d5c99d6cbb83e43b14ad07
Jonathan King <jonathan.j.king@gmail.com>
2015-10-18 17:26:26 +0100

    Added bash-like search history

--- index.js
+++ index.js
@@ -2,24 +2,113 @@
 var tpb = require('thepiratebay'), 
     inquirer = require('inquirer'),
     waiting_for_search = false,
-    spawn = require("child_process").spawn;
+    child = require("child_process"),
+    fs = require("fs"),
+    args = require("yargs")
+        .option("search", {
+            alias: "s",
+            description: "pass search term directly"
+        })
+        .option("all", {
+            alias: "a",
+            description: "play all torrents"
+        })
+        .option("visualise", {
+            alias: "v",
+            description: "play visualisation of music. Requires vsxu_player"
+        })
+        .option("night", {
+            alias: "n",
+            description: "turns on nightaudio. Requires nightaudio"
+        })
+        .help("help")
+        .argv,
+    visualise = false,
+    night = false,
+    play_all_torrents = false,
+    history;
 
-var questions = [{
-    type: "input",
-    name: "name",
-    message: "What do you want to stream?",
-}]
 
-inquirer.prompt([{
-    type: "input",
-    name: "name",
-    message: "What do you want to stream?",
-}], function(search_term) {
-    search(search_term.name)
-})
+try {
+    history = require("./piratestream_history.json")
+    if (!Array.isArray(history))
+        console.log("History file is corrupted. Replacing.");
+} 
+catch(e) {
+    history = []
+}
+
+var history_index = history.length - 1;
+
+if (args.v)
+    visualise = true;
+
+if (args.night)
+    night = true;
+
+if (args.all)
+    play_all_torrents = true;
+
+function start(){
+    if (args.search)
+        search(args.search)
+    else
+        prompt_search()
+}
+
+function prompt_search(){
+    var prompt = inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What do you want to stream?",
+    }], function(search_term) {
+        history_listen()
+        search(search_term.name)
+    })
+    history_listen(prompt)
+}
+
+function history_listen(prompt){
+    var stdin = process.openStdin();
+    salt = prompt; 
+
+    if (prompt){
+        process.stdin.setRawMode(true);    
+        stdin.on('keypress', history_lookup);
+    } else {
+        stdin.removeListener('keypress', history_lookup)
+    }
+
+    function history_lookup(chunk, key){
+        var last_search = history[history_index]
+        if (last_search === undefined)
+            return "History index error"
+        if (key && key.name == 'up') {
+            prompt.rl.line = history[history_index].search
+            if (history_index > 0)
+                history_index--
+        }
+        if (key && key.name == 'down') {
+            prompt.rl.line = history[history_index].search
+            if (history_index < history.length -1)
+                history_index++
+        }
+    }
+
+}
+
 
 function search(name){
     waiting_for_search = true
+    var entry = {
+        "search": name,
+        "date" : new Date()
+    }
+    history.push(entry)
+    var out = JSON.stringify(history)
+    fs.writeFile("piratestream_history.json", out, function(err){
+        if (err) throw "Couldn't write history to file", err
+    })
     searching()
     tpb.search(name, {
         category: '0',
@@ -40,23 +129,73 @@ function searching(){
 }
 
 function choose(results){
+    if (results.length == 0){
+        console.log("No results.")
+        start()
+    }
     waiting_for_search = false;
     var choices = {};
-    for (var i in results) choices[results[i].name + " - " + results[i].seeders + " seeders - " + results[i].size] = results[i].magnetLink;
+    for (var i in results) choices[results[i].name] = results[i].magnetLink;
     inquirer.prompt([{
         type: "list",
         name: "name",
         message: "Choose a torrent",
-        choices: results
+        choices: function(){
+            var arr = []
+            for (var i in results){
+                arr.push({
+                    name: results[i].name + " - " + results[i].seeders + " seeders - " + results[i].size,
+                    value: results[i].name
+                })
+            }
+            return arr
+        }
     }], function(choice){
         var magnet = choices[choice.name]
         if (magnet){
-            var peerflix = spawn("peerflix", [magnet, "-a", "--vlc"])
+            var peerflix = child.spawn("peerflix", [magnet, "-a", "--vlc", "-- --fullscreen"]),
+                end = new RegExp(/and ?\d+ more/),
+                closed = false;
             peerflix.stdout.on('data', function (data) {
+                data = data.toString()
+                    .replace(/^\s*\n/gm, "") 
                 process.stdout.write(data);
+                if (end.test(data.toString()))
+                    process.stdout.write("\033[1;1H")
             });
+            process.on('exit', function(){
+                cleanup()
+            })
+            process.on('close', function(code, signal){
+                cleanup()
+            })
+            process.on('SIGINT', function(code, signal){
+                cleanup()
+            })
+            process.on('SIGTERM', function(code, signal){
+                cleanup()
+            })
+            if (visualise)
+                start_visualisation();
         } else {
             console.log("No magnet link for", choice.name)
         }
     })
 }
+
+function cleanup(){
+    process.stdout.write("\033[2J")
+    if (visualise)
+        child.exec("xrandr", ["--auto"]);
+}
+
+function start_visualisation(){
+    // Wait for VLC to start up
+    setTimeout(function(){
+        child.spawn("vsxu_player",["-f", "-s", "3286x1080"])
+        if (night)
+            child.exec("nightaudio", ["tv"])
+    }, 6000)
+}
+
+start()
